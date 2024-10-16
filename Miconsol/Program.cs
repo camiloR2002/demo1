8783859d22111ef09f092fa1215a26512f2bc7cb
siver <siver@MAAMBOWIN>
2024-10-15 22:41:54 -0500

    Commit

---
+++ Miconsol/Program.cs
@@ -0,0 +1,16 @@
+﻿using Milibrary;
+
+namespace Miconsol
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MessageWriter MyWriter = new MessageWriter();
+            MyWriter.Writer("Hello Writer");
+
+            MessageWriter FWriter = new MessageWriter();
+            FWriter.Writer("Hello Figgle");
+        }
+    }
+}
---
+++ Milibrary/MessageWriter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milibrary
+{
+    public class MessageWriter
+    {
+       public void Writer(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
